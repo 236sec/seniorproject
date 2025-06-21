39ac8c2904ddd1cb081a1d63faa4d0753fefe7ef
jongruk-t <jongruk@primo.mobi>
2025-06-21 23:53:19 +0700

    Add list market api

--- backend/src/gecko/gecko.service.ts
+++ backend/src/gecko/gecko.service.ts
@@ -11,7 +11,7 @@ import { Cache } from 'cache-manager';
 import {
   MarketChartResponse,
   PriceDataPoint,
-  VolumeDataPoint,
+  CryptocurrencyArrayResponse,
 } from './model/gecko.model';
 import { PriceChartPeriod } from './dto/getCoinPriceCharts.dto';
 
@@ -36,7 +36,7 @@ export class GeckoService {
     if (cached) {
       return cached;
     }
-    const result = await this.fetchAndUpdatePricesHours(coidId, period);
+    const result = await this.fetchAndPricesCharts(coidId, period);
     if (!result) {
       throw new InternalServerErrorException('Failed to fetch price data');
     }
@@ -49,7 +49,57 @@ export class GeckoService {
     return priceData;
   }
 
-  async fetchAndUpdatePricesHours(
+  async getCoinMarket() {
+    const cached = await this.cacheManager.get('coin_market');
+    if (cached) {
+      return cached;
+    }
+    const result = await this.fetchListCoinMarket();
+    if (!result) {
+      throw new InternalServerErrorException(
+        'Failed to fetch coin market data',
+      );
+    }
+    await this.cacheManager.set('coin_market', result, 600 * 1000);
+    return result;
+  }
+
+  async fetchListCoinMarket() {
+    try {
+      const response = await axios.get<CryptocurrencyArrayResponse>(
+        'https://api.coingecko.com/api/v3/coins/markets',
+        {
+          params: {
+            vs_currency: 'usd',
+            order: 'market_cap_desc',
+            per_page: 30,
+            page: 1,
+            sparkline: false,
+            price_change_percentage: '1h,24h,7d',
+          },
+          headers: {
+            accept: 'application/json',
+            'x-cg-demo-api-key': this.API_KEY ? this.API_KEY : '',
+          },
+        },
+      );
+
+      if (response.status !== 200) {
+        throw new InternalServerErrorException(
+          'Failed to fetch market data from CoinGecko',
+        );
+      }
+
+      this.logger.log(`Fetched market data points: ${response.data.length}`);
+
+      return response.data;
+    } catch (error) {
+      this.logger.error('Failed to fetch market data', error);
+      throw new InternalServerErrorException('Failed to fetch market data');
+    }
+  }
+
+  async fetchAndPricesCharts(
     coinId: string = 'bitcoin',
     period: PriceChartPeriod = PriceChartPeriod.H24,
   ) {
